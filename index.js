73f0332cb8d3aa694b5961358b450e5cf49d166c
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-12-08 18:23:36 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
-const express = require('express')
-const fs = require('fs')
+const express = require('express');
+const morgan = require("morgan");
+const fs = require('fs');
 
-const app = express()
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
 
 app.post('/canciones', (req, res) => {
-    console.log(`recibe datos de cancion y agrega al repertorio`)
+    const data = JSON.parse(fs.readFileSync(`repertorio.json`, 'utf8'));
+    const newData = { id: data.length + 1, ...req.body };
+    fs.writeFileSync(`repertorio.json`, JSON.stringify([...data, newData], null, 4))
+
+    res.json({
+        message: "Producto creado"
+    });
 })
 
 app.get('/canciones', (req, res) => {
-    console.log(`Devuelve un JSON con las canciones registradas en el repertorio`)
+    res.sendFile('./repertorio.json', {
+        root: __dirname
+    });
 })
 
 app.put('/canciones/:id', (req, res) => {
-    console.log(`Recibe los datos de una canción que se desea editar y la actualiza manipulando el JSON local.`)
     const { id } = req.params
-    console.log(id)
+    const newData = req.body
+    let data = JSON.parse(fs.readFileSync(`repertorio.json`, 'utf8'));
+
+    const dataFound = data.find((e) => e.id === parseInt(id))
+
+    if (!dataFound)
+        return res.status(404).json({
+            message: "Producto no encontrado"
+        })
+
+    data = data.map((e) => e.id === parseInt(id)
+        ? { ...e, ...newData }
+        : e)
+
+    fs.writeFileSync(`repertorio.json`, JSON.stringify(data, null, 4))
+
+    res.status(202).json({
+        message: "Producto actualizado"
+    });
+
 })
 
 app.delete('/canciones/:id', (req, res) => {
-    console.log(`Recibe por queryString el id de una canción y la elimina del repertorio.`)
     const { id } = req.params
-    console.log(id)
+    let data = JSON.parse(fs.readFileSync(`repertorio.json`, 'utf8'));
+
+    const dataFound = data.find((e) => e.id === parseInt(id))
+
+    if (!dataFound)
+        return res.status(404).json({
+            message: "Producto no encontrado"
+        })
+
+    data = data.filter((e) => e.id !== parseInt(id))
+
+    fs.writeFileSync(`repertorio.json`, JSON.stringify(data, null, 4))
+
+    res.status(202).json({
+        message: "Producto eliminado"
+    });
 })
 
 app.use((req, res) => {
