cda60d4441036401b2dd0c065dd60227a29f6ba4
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-12-14 16:08:48 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const morgan = require("morgan");
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const archivo = path.join(__dirname, '/repertorio.json')
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.send(`<h1> Well come to Canciones </h1>`);
+// })
+
+app.get('/canciones', (req, res) => {
+    res.sendFile('./repertorio.json', {
+        root: __dirname
+    });
+})
+
+
+app.post('/canciones', (req, res) => {
+    const data = JSON.parse(fs.readFileSync(archivo), 'utf8');
+    console.log(__dirname)
+    const newData = { id: data.length + 1, ...req.body };
+    fs.writeFileSync(archivo, JSON.stringify([...data, newData], null, 4))
+
+    res.json({
+        message: "Producto creado"
+    });
+})
+
+
+app.put('/canciones/:id', (req, res) => {
+    const { id } = req.params
+    const newData = req.body
+    let data = JSON.parse(fs.readFileSync(archivo), 'utf8');
+
+    const dataFound = data.find((e) => e.id === parseInt(id))
+
+    if (!dataFound)
+        return res.status(404).json({
+            message: "Producto no encontrado"
+        })
+
+    data = data.map((e) => e.id === parseInt(id)
+        ? { ...e, ...newData }
+        : e)
+
+    fs.writeFileSync(archivo, JSON.stringify(data, null, 4))
+
+    res.status(202).json({
+        message: "Producto actualizado"
+    });
+
+})
+
+
+app.delete('/canciones/:id', (req, res) => {
+    const { id } = req.params
+    let data = JSON.parse(fs.readFileSync(archivo), 'utf8');
+
+    const dataFound = data.find((e) => e.id === parseInt(id))
+
+    if (!dataFound)
+        return res.status(404).json({
+            message: "Producto no encontrado"
+        })
+
+    data = data.filter((e) => e.id !== parseInt(id))
+
+    fs.writeFileSync(archivo, JSON.stringify(data, null, 4))
+
+    res.status(202).json({
+        message: "Producto eliminado"
+    });
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res) => {
+    res.status(404).send('<h1>Not Found</h1>')
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`)
